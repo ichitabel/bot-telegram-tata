2856be9b4d4a4fc26934916856f5dabed8f47d8e
ichitabel <67168951+ichitabel@users.noreply.github.com>
2020-06-20 17:17:48 +0200

    Update Registro.py

--- Registro.py
+++ Registro.py
@@ -6,7 +6,6 @@ class Registro():
 
     def add(self,info):
         gana = False
-        self.grupos
         id = Info_Mensaje(info).id_chat
         grupo = filter(lambda n: n == id, self.grupos)
         if grupo == None:
