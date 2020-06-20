52d74b7f17bb0354c268e6c145206a3ac430136a
ichitabel <67168951+ichitabel@users.noreply.github.com>
2020-06-20 16:35:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,42 +8,41 @@ import requests
 from flask import Flask, request
 from datetime import datetime,time
 
+
 BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'  # <-- add your telegram token as environment variable
 
-class Bot():
-
-    app = Flask(__name__)
-    def __init__(self):
-        self.num_pole = 0
+app = Flask(__name__)
+num_pole = 0
 
 
 
-    @app.route('/',methods=['Post'])
-    def main(self):
+@app.route('/',methods=['Post'])
+def main():
         sms = request.json
-        info = self.info_mensaje(sms)
+        info = info_mensaje(sms)
         if not info.is_bot and info.tipo_sms == "texto":
-            if str(self.leer_mensaje(sms)).lower() == "!polerank":
-                self.enviar_mensaje(info.id_chat,
+            if str(leer_mensaje(sms)).lower() == "!polerank":
+                enviar_mensaje(info.id_chat,
                                            " No tenemos rango Equis De")
                 #self.enviar_mensaje(Info_Mensaje(info).id_chat, self.puntuacion(Info_Mensaje(info).id_chat))
-            elif str(self.leer_mensaje(sms)).lower() == "pole":
-                date = self.unix_date(info.date)
-                pole = self.is_pole(date)
+            elif str(leer_mensaje(sms)).lower() == "pole":
+                date = unix_date(info.date)
+                pole = is_pole(date)
                 if pole != -1:
-                    if pole != self.num_pole:
+                    if pole != num_pole:
                         registro = Registro()
-                        self.num_pole = pole
+                        num_pole = pole
                     if (registro.add(info)):
-                        self.enviar_mensaje(info.id_chat,
+                        enviar_mensaje(info.id_chat,
                                             info.persona + " ha ganado la pole XD")
                         #Servicios.add(Info_Mensaje(info).id_chat, Info_Mensaje(info).id_persona)
                     else :
-                        self.enviar_mensaje(info.id_chat,
+                        enviar_mensaje(info.id_chat,
                                            " Te mamaste")
                 else:
-                    self.enviar_mensaje(info.id_chat,
+                    enviar_mensaje(info.id_chat,
                                            " No son horas de pole idiota ")
+        return ''
 
 
 
@@ -56,21 +55,20 @@ class Bot():
   #      for persona in grupo.personas:
    #         text + persona.nombre + " " + str(persona.cant)+"\n"
 
-    def leer_mensaje(self, mensaje):
-        texto = mensaje["message"]["text"]
+def leer_mensaje(mensaje):
+        texto = mensaje['message']['text']
         return texto
 
-    def enviar_mensaje(self,idChat, texto):
-        json_data = {
+def enviar_mensaje(idChat, texto):
+    json_data = {
         "chat_id": idChat,
         "text": texto,
-        }
-        message_url = BOT_URL + 'sendMessage'
-        requests.post(message_url, json=json_data)
-         
-    
+    }
+    message_url = BOT_URL + 'sendMessage'
+    requests.post(message_url, json=json_data)
+    return ''
 
-    def info_mensaje(self, mensaje):
+def info_mensaje(mensaje):
         if "text" in mensaje["message"]:
             tipo_sms ="texto"
         elif "sticker" in  mensaje["message"]:
@@ -81,17 +79,18 @@ class Bot():
             tipo_sms = "foto"
         else:
             tipo_sms = "otro"
-        update_id = mensaje["update_id"]
-        persona = mensaje["message"]["from"]["first_name"]
-        id_persona = mensaje["message"]["from"]["id"]
-        bot = mensaje["message"]["from"]["is_bot"]
-        chat = mensaje["message"]["chat"]["first_name"]
-        id_chat = mensaje["message"]["chat"]["id"]
-        tipo_chat = mensaje["message"]["chat"]["type"]
-        date = mensaje["message"]["date"]
+        update_id = mensaje['update_id']
+        persona = mensaje['message']['from']['first_name']
+        id_persona = mensaje['message']['from']['id']
+        bot = mensaje['message']['from']['is_bot']
+        #chat = mensaje['message']['chat']['first_name']
+        chat = "el pingas"
+        id_chat = mensaje['message']['chat']['id']
+        tipo_chat = mensaje['message']['chat']['type']
+        date = mensaje['message']['date']
         return Info_Mensaje( persona, id_persona , bot, chat, id_chat, tipo_chat, tipo_sms, date,update_id)
 
-    def is_pole(self,date):
+def is_pole(date):
         lista = [[time(3),time(8,59)],[time(9),time(14,59)],[time(15),time(20,59)],[time(21),time(23,59)]]
         pole = -1
         for i in range(0, lista.__len__()-1):
@@ -100,8 +99,8 @@ class Bot():
                 break
         return pole
 
-    def unix_date(self,fecha):
+def unix_date(fecha):
         return datetime.fromtimestamp(fecha).time()
 
-    if __name__ == '__main__':  
+if __name__ == '__main__':  
         app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
