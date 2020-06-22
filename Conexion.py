651d48685a7893be14b88a73611f75f44008e7b5
ichitabel <67168951+ichitabel@users.noreply.github.com>
2020-06-22 17:47:26 +0200

    Update Conexion.py

--- Conexion.py
+++ Conexion.py
@@ -5,7 +5,7 @@ import Singleton
 class Conexion:
     def __init__(self):
         try:
-            self.miConexion = psycopg2.connect(dbname="dn43sr6l9fe8j", user="kmmfirqlfmuxce", password="8485bb0c2d4a98bf8993e3ad7ac24fc42e722ee5f20fa6d919b45e537b7e6034",host="ec2-34-230-231-71.compute-1.amazonaws.com")
+            self.miConexion = psycopg2.connect(os.environ["DATABASE_URL"])
         except:
             print("Error en la base de datos")
 
