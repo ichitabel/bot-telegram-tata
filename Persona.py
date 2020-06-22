1915acd91f36b5ecbc39cde618c762602b4e4b52
ichitabel <67168951+ichitabel@users.noreply.github.com>
2020-06-22 04:12:10 +0200

    Update Persona.py

--- Persona.py
+++ Persona.py
@@ -1,5 +1,6 @@
 class Persona():
-    def __init__(self, grupo, id,cant):
+    def __init__(self, grupo, id,cant,nombre_persona):
         self.id_grupo = grupo
         self.id_persona = id
         self.cantidad = cant
+        self.nombre_persona = nombre_persona
