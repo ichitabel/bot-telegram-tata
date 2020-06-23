d9ba35eab0072f6867669fef398ff5b8d42dfa42
ichitabel <67168951+ichitabel@users.noreply.github.com>
2020-06-23 21:44:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -122,7 +122,7 @@ def info_mensaje(mensaje):
     return Info_Mensaje( persona, id_persona , bot, chat, id_chat, tipo_chat, tipo_sms, date,update_id)
 
 def is_pole(date):
-    lista = [[time(0,0),time(1,33)],[time(1,33),time(7,33)],[time(7,33),time(13,33)],[time(13,33),time(19,33)],[time(19,33),time(0,0)]]
+    lista = [[time(0,0),time(1,33)],[time(1,33),time(7,33)],[time(7,33),time(13,33)],[time(13,33),time(19,33)],[time(19,33),time(23,59,999999)]]
     pole = -1
     for i in range(0, lista.__len__()):
         if date >= lista[i][0] and date < lista[i][1]:
